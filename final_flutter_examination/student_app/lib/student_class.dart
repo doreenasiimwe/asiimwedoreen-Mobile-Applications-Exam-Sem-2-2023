92375aab7acb0d98c705de90c3a342613c9f5ce6
doreenasiimwe <doreenasiimwe5@gmail.com>
2023-11-30 13:59:50 +0300

    flutter

--- final_flutter_examination/student_app/lib/student_class.dart
+++ final_flutter_examination/student_app/lib/student_class.dart
@@ -1,2 +1,14 @@
+Class Student{
+  String student_name;
+  int student_age;
 
+  List<double> grades;
+
+  // Constructor
+  Student(String name, int age, List<double> grades) {
+    this.name = name;
+    this.age = age;
+    this.grades = grades;
+  }
+}
 
